c0987319c005e771e4933fe239c79e3d3608dde3
Jingzhi <2268236614@qq.com>
2024-11-09 20:53:26 +0800

    feat: show medals

--- assets/scripts/UI/GameOverUI.ts
+++ assets/scripts/UI/GameOverUI.ts
@@ -13,6 +13,8 @@ export class GameOverUI extends Component {
     bestScoreLabel: Label = null;
     @property(Sprite)
     newSprite: Sprite = null;
+    @property([Node])
+    medalArray: Node[] = [];
     
     public show(curScore: number, bestScore: number) {
         this.node.active = true;
@@ -24,6 +26,11 @@ export class GameOverUI extends Component {
         } else {
             this.newSprite.node.active = false;
         }
+
+        // 0-9
+        let index = Math.floor(curScore / 5);
+        if (index > 3) index = 3;
+        this.medalArray[index].active = true;
     }
 
     public hide() {
